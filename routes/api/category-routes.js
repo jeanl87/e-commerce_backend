5bc272903648bee7f5f59a3810884cfac5580e5d
Lisa Jean <MsJean@Lisas-Air.fios-router.home>
2022-03-16 08:01:09 -0400

    correct folders, added .evv, continued working on routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "category_id"],
+      },
+    ],
+  }).then((categories) => {
+    res.json(categories);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  }).then((category) => {
+    res.json(category);
+  });
+});
+
+router.post("/", (req, res) => {
+  // create a new category
+  Category.create(req.body).then((newcategory) => {
+    res.json(newcategory);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  // delete a category by its `id` value
+  Category.destory;
+});
+
+module.exports = router;
